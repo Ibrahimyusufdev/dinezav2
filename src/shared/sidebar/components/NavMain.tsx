826f5a7339f3a8da872d7454a0900ef0b929ba0f
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2026-03-16 01:01:45 +0100

    RepoSetup(Made sidebar to close upon route clicking on mobile)

--- src/shared/sidebar/components/NavMain.tsx
+++ src/shared/sidebar/components/NavMain.tsx
@@ -1,5 +1,5 @@
 // Shadcn ui
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
 
 import { NavLink } from "react-router-dom";
 
@@ -8,10 +8,12 @@ import { useAuthStore } from "@/features/auth";
 
 const NavMain = () => {
   // Getting user data from auth store, so you can access the role signed it with
-  const user = useAuthStore((state) => state.user)
+  const user = useAuthStore((state) => state.user);
 
   // Func to get sidebar based on user role form auth store user data
   const config = getSidebarConfig(user.role);
+
+  const {setOpenMobile} = useSidebar()
 
   return (
     <SidebarMenu>
@@ -25,6 +27,7 @@ const NavMain = () => {
                 ? "bg-blue-50 text-blue-700"
                 : "text-gray-700 hover:bg-gray-100 hover:text-gray-900"
             }
+            onClick={() => setOpenMobile(false)}
           >
             <SidebarMenuButton tooltip={item.title} className="cursor-pointer px-4 py-5">
               <item.icon />
